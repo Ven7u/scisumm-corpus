cb6e86e5b087dc37cfb03bcb69bd9372662b93f7
Alessandro Farasin <alessandro.farasin@live.it>
2018-01-31 16:42:56 +0100

    init

---
+++ code/test1.py
@@ -0,0 +1,124 @@
+from sklearn.decomposition import TruncatedSVD
+from sklearn.random_projection import sparse_random_matrix
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import pandas as pd
+from bs4 import BeautifulSoup
+import nltk
+import lxml
+import os
+
+import text_processing as tp
+
+#input_folder = "/Users/francescoventura/Documents/POLI/Corsi/dottorato/text mining and analytics/exam/scisumm-corpus-master/"
+input_folder = "../"
+data_folder = "data/"
+
+def get_set_folder(type = "Training", year = "2017"):
+    return  input_folder + data_folder + type + "-Set-" + year + "/"
+
+def get_ref_ids():
+
+    return ["C00-2123"]
+
+def get_cit_ids():
+
+    return ["C02-1050",
+    "C04-1091",
+    "E03-1007",
+    "E06-1004",
+    "H01-1062",
+    "J03-1005",
+    "J04-2003",
+    "J04-4002",
+    "N03-1010",
+    "P01-1027",
+    "P03-1039",
+    "W01-0505",
+    "W01-1404",
+    "W01-1407",
+    "W01-1408",
+    "W02-1020"]
+
+
+
+
+def load_ref_xml_source(set_folder,ref_id):
+    return load_source(set_folder+ref_id+"/Reference_XML/"+ref_id+".xml",ref_id)
+
+
+def load_cit_xml_source(set_folder, ref_id, cit_id):
+    return load_source(set_folder+ref_id+"/Citance_XML/"+cit_id+".xml",cit_id)
+
+
+def load_source(source, doc_name):
+    file = source #+ str(id) + "/" + str(id) + "/DEFAULT/Part3DP.xml"  # sys.argv[1]
+    handler = open(file).read()
+    soup = BeautifulSoup(handler, 'xml')
+
+    sentences = soup.findAll("S")
+    sentences_dicts = []
+    for s in sentences:
+        sentences_a = {}
+        sentences_a["doc"] = doc_name
+        sentences_a["sid"] = s["sid"]
+        #sentences_a["ssid"] = s["ssid"]
+        sentences_a["tagged_text"] = s
+        sentences_a["text"] = s.contents[0]
+        sentences_dicts.append(sentences_a)
+    return sentences_dicts
+
+def load_ref_cit_tuple():
+    return ""
+
+def test_1():
+
+    for ref_id in get_ref_ids():
+        for cit_id in get_cit_ids():
+
+            #s1 = load_source(source_1, "CIT_PAPER_1")
+            #s2 = load_source(source_2, "REF_PAPER")
+
+            s1 = load_ref_xml_source(get_set_folder(),ref_id)
+            s2 = load_cit_xml_source(get_set_folder(),ref_id,cit_id)
+
+            sentences_df = pd.DataFrame(data=s1 + s2)
+            sentences_df["token_counts"] = sentences_df.apply(tp.tokens_count, axis=1)
+            sentences_df = sentences_df[((sentences_df["token_counts"] > 10) & (sentences_df["token_counts"] <= 70))]
+
+            print(sentences_df)
+
+
+
+            vocabulary, tf = tp.preprocess(sentences_df)
+
+            lsi_terms, lsi_rows = tp.run_lsi(tf,vocabulary,sentences_df)
+
+            ref_scores, cit_scores = tp.get_ref_and_cit_scores(lsi_rows,ref_id, cit_id)
+
+            cosine_scores = tp.get_cosine_similarities(ref_scores, cit_scores)
+
+            result = tp.get_final_scores(cosine_scores, ref_scores, cit_scores)
+
+            print(result)
+
+
+            out_folder = "out/" + ref_id + "/" + cit_id + "/"
+            out_file = "related_sentences.csv"
+
+            if not os.path.exists(out_folder):
+                os.makedirs(out_folder)
+
+            result.to_csv(out_folder+out_file, index=False)
+
+
+
+def main():
+    test_1()
+
+    return 0
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/text_processing.py
@@ -0,0 +1,121 @@
+from collections import Counter
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import pandas as pd
+import sklearn.metrics.pairwise as sklm
+import numpy as np
+import operator
+
+
+stop_words = set(stopwords.words('english'))
+
+
+
+def tokens_count(row):
+    #print(row["text"])
+    tokenizer = nltk.RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(row['text'])
+    return len(tokens)
+
+# print(stop_words)
+def row_preprocess(row):
+    tokenizer = nltk.RegexpTokenizer(r'\w+')
+    # print(row)
+    tokens = tokenizer.tokenize(row)
+    # tokens = [nltk.word_tokenize(t) for t in tokens]
+    # print(tokens)
+    filtered_tokens = [t.lower() for t in tokens if t.lower() not in stop_words]
+    # print(filtered_tokens)
+
+    lmtzr = WordNetLemmatizer()
+    lems = [lmtzr.lemmatize(t) for t in filtered_tokens]
+    return " ".join(lems)
+
+
+
+def preprocess(sentences_df):
+    count_vect = CountVectorizer(stop_words="english", preprocessor=row_preprocess)
+    X_train_counts = count_vect.fit_transform(sentences_df.text)
+    # print(X_train_counts.shape)
+    # print(count_vect.get_feature_names())
+    vocabulary = pd.DataFrame(dict(count_vect.vocabulary_), index=["index"]).T.reset_index()
+    vocabulary.columns = ["word", "index"]
+    vocabulary = vocabulary.set_index("index")
+
+    tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+    X_train_tf = tf_transformer.transform(X_train_counts)
+    print(X_train_tf.shape)
+
+    return (vocabulary, X_train_tf)
+
+
+
+
+def run_lsi(tf, vocabulary, sentences_df):
+
+
+    svd = TruncatedSVD(n_components=50)
+    svd_matrix_terms = svd.fit_transform(tf.T)
+    svd_matrix_rows = svd.fit_transform(tf)
+
+    print(svd_matrix_terms.shape)
+    print(svd_matrix_rows.shape)
+
+    lsi_terms = pd.DataFrame(svd_matrix_terms)
+    lsi_terms = pd.merge(vocabulary, lsi_terms, left_index=True, right_index=True).reset_index().set_index(["index", "word"])
+
+
+    lsi_rows = pd.DataFrame(svd_matrix_rows)
+    lsi_rows = pd.merge(sentences_df[["doc", "sid", "text"]], lsi_rows, left_index=True,
+                        right_index=True).reset_index().set_index(["index", "doc", "text"])
+    return (lsi_terms, lsi_rows)
+
+
+def get_ref_and_cit_scores(lsi_rows,ref_id,cit_id):
+    scores = lsi_rows.reset_index()
+    ref_scores = scores[scores["doc"] == ref_id].reset_index()
+    ref_scores["sid"] = pd.to_numeric(ref_scores["sid"])
+
+    cit_scores = scores[scores["doc"] == cit_id].reset_index()
+    cit_scores["sid"] = pd.to_numeric(cit_scores["sid"])
+
+    return (ref_scores, cit_scores)
+
+
+def get_cosine_similarities(ref_scores,cit_scores):
+    ref_ranks = ref_scores[["index", "doc", "text", "sid"]]
+    cosine_scores = []
+
+    x = ref_scores.iloc[:, 5:]
+    y = cit_scores.iloc[:, 5:]
+
+    print(x)
+    print(y)
+    cosine_scores = sklm.cosine_similarity(x, y)
+    print(cosine_scores.shape)
+    return cosine_scores
+
+
+def get_final_scores(cosine_scores,ref_scores,cit_scores):
+
+
+    similarity_max = []
+    for r in range(len(cosine_scores)):
+        cit_index, value = max(enumerate(cosine_scores[r]), key=operator.itemgetter(1))
+        similarity_max.append(
+            {"sid": ref_scores.iloc[r]["sid"], "cit_sid": cit_scores.iloc[cit_index]["sid"], "score": value})
+
+    similarity_scores = pd.DataFrame(similarity_max)
+    results = similarity_scores.merge(ref_scores[["doc", "sid", "text"]], on="sid").merge(cit_scores[["doc", "sid", "text"]],
+                                                                                   left_on="cit_sid",
+                                                                                   right_on="sid").sort_values("score",
+                                                                                                               ascending=False)
+    #print(results)
+    results.columns = ["cit_sid","score","ref_sid","ref_doc","ref_text","cit_doc","cit_sid_2","cit_text"]
+    results = results[["ref_doc","ref_sid","ref_text","cit_doc","cit_sid","cit_text","score"]]
+    return results[0:15]
+
+
